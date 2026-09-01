2c6914e6eeb332018daf8ac7de07bf81ad25d2c6
Den <denkalenichenko@gmail.com>
2026-09-02 02:43:25 +0300

    perf(store): build UUIDv7 strings with hex.Encode
    fmt.Sprintf("%x-%x-...") allocates and runs the format machinery for
    every ID; encoding/hex into a fixed 36-byte array does the same work
    without either. Output is byte-identical.

--- internal/store/id.go
+++ internal/store/id.go
@@ -2,7 +2,7 @@ package store
 
 import (
 	"crypto/rand"
-	"fmt"
+	"encoding/hex"
 	"time"
 )
 
@@ -25,5 +25,17 @@ func NewID() string {
 	b[6] = 0x70 | b[6]&0x0f
 	b[8] = 0x80 | b[8]&0x3f
 
-	return fmt.Sprintf("%x-%x-%x-%x-%x", b[0:4], b[4:6], b[6:8], b[8:10], b[10:16])
+	var buf [36]byte
+
+	hex.Encode(buf[0:8], b[0:4])
+	buf[8] = '-'
+	hex.Encode(buf[9:13], b[4:6])
+	buf[13] = '-'
+	hex.Encode(buf[14:18], b[6:8])
+	buf[18] = '-'
+	hex.Encode(buf[19:23], b[8:10])
+	buf[23] = '-'
+	hex.Encode(buf[24:36], b[10:16])
+
+	return string(buf[:])
 }
